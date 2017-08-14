0c71215973fa51a70537615dd3f5de4bc8b3e33b
getian124 <3044327581@qq.com>
2017-08-14 14:13:49 +0800

    Signed-off-by: getian124 <3044327581@qq.com> 首页图片信息

--- Public/Home/cartoon/common/js/common.js
+++ Public/Home/cartoon/common/js/common.js
@@ -1,11 +1,20 @@
 
-//点击弹出登录模态框
-$("#entry_lg").click(function(){
-    $("#login_second").css('display','block');
+//登录头像的放大效果
+$("#person_show").mouseover(function(){
+    $(this).addClass("person_show");
+    $("#person_center_pst").animate({height:"220px"},200)
+});
+$("#person_center_pst"). mouseleave(function(){
+    $("#person_show").removeClass("person_show");
+    $("#person_center_pst").animate({height:"0px"},200)
+});
+//登录
+$('#entry_lg').click(function(){
+    $("#login_second").css('display','block')
+});
+$('#login_book').click(function(){
+    $("#login_second").css('display','block')
 });
 $('#close_login').click(function(){
-    $("#login_second").css('display','none');
+    $("#login_second").css('display','none')
 });
-
-
-
--- Public/Home/cartoon/js/index.js
+++ Public/Home/cartoon/js/index.js
@@ -4,25 +4,25 @@ jQuery("#section_three_down").slide({
     effect:"fade",
     autoPlay:false
 });
-//登录头像的放大效果
-$("#person_show").mouseenter(function(){
-    $(this).addClass("person_show");
-    $("#person_center_pst").animate({height:"220px"},200)
-});
-$("#person_center_pst"). mouseleave(function(){
-    $("#person_show").removeClass("person_show");
-    $("#person_center_pst").animate({height:"0px"},200)
-});
-//登录
-$('#entry_lg').click(function(){
-   $("#login_modal").css('display','block')
-});
-$('#login_book').click(function(){
-    $("#login_second").css('display','block')
-});
-$('#close_login').click(function(){
-    $("#login_second").css('display','none')
-});
+////登录头像的放大效果
+//$("#person_show").mouseenter(function(){
+//    $(this).addClass("person_show");
+//    $("#person_center_pst").animate({height:"220px"},200)
+//});
+//$("#person_center_pst"). mouseleave(function(){
+//    $("#person_show").removeClass("person_show");
+//    $("#person_center_pst").animate({height:"0px"},200)
+//});
+////登录
+//$('#entry_lg').click(function(){
+//   $("#login_modal").css('display','block')
+//});
+//$('#login_book').click(function(){
+//    $("#login_second").css('display','block')
+//});
+//$('#close_login').click(function(){
+//    $("#login_second").css('display','none')
+//});
 
 //侧边导航
 $(document).scroll(function(){
